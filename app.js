bc3d5e71ddc5485e3e4da98cb00a6fe77f887644
Nikhil Sharma <97504900+NikhilSHarma8429@users.noreply.github.com>
2022-11-28 10:26:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,59 @@
+import cors from 'cors';
+import ytdl from 'ytdl-core';
+
+const express = require('express');
+
+const app = express();
+
+app.use(cors('*'));
+
+app.get('/check-download', async (req, res, next) => {
+  try {
+    const { URL } = req.query;
+    const {
+      player_response: {
+        videoDetails: { title, author },
+      },
+    } = await ytdl.getBasicInfo(URL);
+    res.json({
+      status: true,
+      title,
+      author,
+    });
+    next();
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+app.get('/download', async (req, res) => {
+  try {
+    const {
+      URL, downloadFormat, quality, title,
+    } = req.query;
+    if (downloadFormat === 'audio-only') {
+      res.setHeader(
+        'Content-Disposition',
+        `attachment; filename=${title}.mp3`,
+      );
+      ytdl(URL, {
+        filter: format => format.container === 'm4a' && !format.encoding,
+        quality: quality === 'high' ? 'highest' : 'lowest',
+      }).pipe(res);
+    } else {
+      res.header(
+        'Content-Disposition',
+        `attachment; filename="${title}.mp4"`,
+      );
+      ytdl(URL, {
+        filter: downloadFormat === 'video-only' ? 'videoonly' : 'audioandvideo',
+        quality: quality === 'high' ? 'highestvideo' : 'lowestvideo',
+      }).pipe(res);
+    }
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+// eslint-disable-next-line no-console
+app.listen({ port: 8000 }, () => console.log('🚀 Server ready at http://localhost:8000'));
